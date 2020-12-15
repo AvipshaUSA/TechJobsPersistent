1df5a3d3fe050cf63494ef0e94e570b1aa3f5f23
AvipshaUSA <68029807+AvipshaUSA@users.noreply.github.com>
2020-12-14 18:16:35 -0600

    Part2 view model complete

---
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+       
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be 3 and 50 charecters.")]
+        public string  Name { get; set; }
+
+        [Required(ErrorMessage = "Location is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Location must be 3 and 50 charecters.")]
+        public string Location { get; set; }
+
+        public AddEmployerViewModel()
+        {
+
+            //Empty constructor
+        }
+
+    }
+}
