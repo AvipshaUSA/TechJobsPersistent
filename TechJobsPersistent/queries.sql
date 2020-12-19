859b5f6b2b18c7f1cbb1386c37dcd5be5603fa53
AvipshaUSA <68029807+AvipshaUSA@users.noreply.github.com>
2020-12-19 13:52:57 -0600

    SQL quaries Added- Assignment complete

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,12 @@
 --Part 1
+describe jobs;
+
 
 --Part 2
+select Name from  employers where Location = "St. Louis City";
+
 
 --Part 3
+
+select s.Name, s.Description from skills s, jobs j where s.Id =j.Id order by s.Name asc;
 
